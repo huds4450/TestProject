b9a06948f8c91306e92de4a35cc446bcf2388da2
Maximus <huds4450@pacificu.edu>
2021-09-06 14:46:31 -0700

    initial commit

---
+++ src/testCode.c
@@ -0,0 +1,25 @@
+/****************************************************************************
+ File name:		HelloWorld.c
+ Author:     	Maximus Hudson
+ Date:       	09/06/2021
+ Class:      	CS300
+ Assignment: 	C Examples
+ Purpose:    	Simple C Demo
+ ****************************************************************************/
+
+#include <stdio.h>
+
+/****************************************************************************
+ Function:			main
+
+ Description:		Display hello world
+
+ Parameters:		None
+
+ Returned:			0 for Success
+ ****************************************************************************/
+int main ()
+{
+	printf ("Hello World!");
+	return 0;
+}
